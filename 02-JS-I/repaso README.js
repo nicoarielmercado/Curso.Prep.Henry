229f74a83ed87d7c792bee2be165ab57151cd96b
nicoarielmercado <nicomercado03@outlook.com>
2021-11-22 19:00:44 -0300

    Subida a unidad 1

--- 02-JS-I/repaso README.js
+++ 02-JS-I/repaso README.js
@@ -76,3 +76,22 @@ function combinarNombres2(nombre, apellido) {
     return "Hola "+nombre+" "+apellido+"!";
   }
   console.log(obtenerSaludo ("Nico","Mercado"));
+
+  ///////////////////////////////////
+
+  function esVocal(letra){
+    //Escribe una función que reciba una letra y, si es una vocal, muestre el mensaje “Es vocal”. 
+    //Verificar si el usuario ingresó un string de más de un carácter, en ese caso, informarle 
+    //que no se puede procesar el dato mediante el mensaje "Dato incorrecto".
+    // Si no es vocal, tambien debe devolver "Dato incorrecto".
+    //Escribe tu código aquí
+   
+if (letra.length > 1) {
+  return "Dato incorrecto"
+}
+if (letra === "a" || letra === "e" ||letra === "i" ||letra === "o" ||letra === "u") {
+  return "Es vocal"
+}
+return "Dato incorrecto"
+}
+
